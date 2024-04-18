7e165c738ec53ce0aaceff9ce5e1f975e6ec1a8c
anamsilva1981 <ana.msilva@hotmail.com>
2024-04-18 11:02:02 -0300

    feat: Two Way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwoWayBindingComponent,
+    DataBindingComponent,
+    EventBindingComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/two-way-binding/two-way-binding.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-way-binding',
+  template: `
+    <h2>Two Way binding</h2>
+    <input [(ngModel)]="nome">
+    {{ nome }}
+  `,
+})
+export class TwoWayBindingComponent {
+nome: any;
+
+}
